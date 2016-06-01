38098f9036c05492ababfe236aa7d63cb504336f
Wasim <wasimbloch@gmail.com>
2016-06-01 20:52:15 +0530

    Fixed the issue with deployment and updated script file

--- Demos/2_NSG/NSGDemoSolution/NSGDemoSolution/Scripts/Deploy-AzureResourceGroup.ps1
+++ Demos/2_NSG/NSGDemoSolution/NSGDemoSolution/Scripts/Deploy-AzureResourceGroup.ps1
@@ -4,7 +4,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'NSGDemoSolution',
+    [string] $ResourceGroupName = 'TM-Demo-Solution',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageAccountResourceGroupName, 
